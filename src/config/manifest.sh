7c307371805b24a40f2ed973421e14f5c60f3a8c
notoriouslogank <logan.aker1989@gmail.com>
2023-05-16 22:50:32 -0400

    Create list of packages to install.

---
+++ src/config/manifest.sh
@@ -0,0 +1,3 @@
+APT_PKG="curl wget htop bpytop nmon ranger git lolcat neofetch"
+PACMAN_PKG="curl wget htop bpytop nmon ranger git lolcat neofetch"
+YUM_PKG="curl wget htop bpytop nmon ranger git lolcat neofetch"
