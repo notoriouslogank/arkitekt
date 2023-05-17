7b0cd1f595099d2b8744e8a8a9fb75fa85ed9c04
notoriouslogank <logan.aker1989@gmail.com>
2023-05-16 22:51:20 -0400

    Create basic package install script.

---
+++ src/arkitekt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Get relevant application lists
+source config/manifest.sh
+
+YUM_CMD=$(which yum)
+APT_CMD=$(which apt)
+PACMAN_CMD=$(which pacman)
+
+# Determine which package manage is being used and
+# install necessary packages
+if [[ ! -z $APT_CMD ]]; then
+    apt update && apt full-upgrade -y
+    apt install $APT_PKG
+elif [[ ! -z $PACMAN_CMD ]]; then
+    pacman -Syu
+    pacman -Su $PACMAN_PKG
+elif [[ ! -z $YUM_CMD ]]; then
+    yum install $YUM_PKG
+else
+    echo "ERROR: Can't install package(s). Verify correct package manager."
+    exit 1;
+fi
