1d07020ae2c3b2ca02310f9411ef6d9607dbf35f
notoriouslogank <logan.aker1989@gmail.com>
2023-05-20 19:06:29 -0400

    Minor formatting fixes.

--- src/arkitekt.sh
+++ src/arkitekt.sh
@@ -23,18 +23,18 @@ function backup_dotfiles {
 function pkg_check_and_install {
     # Determine which package manage is being used and
     # install necessary packages
-if [[ ! -z $APT_CMD ]]; then
+    if [[ ! -z $APT_CMD ]]; then
     apt update && apt full-upgrade -y
     apt install $APT_PKG
-elif [[ ! -z $PACMAN_CMD ]]; then
+    elif [[ ! -z $PACMAN_CMD ]]; then
     pacman -Syu
     pacman -Su $PACMAN_PKG
-elif [[ ! -z $YUM_CMD ]]; then
+    elif [[ ! -z $YUM_CMD ]]; then
     yum install $YUM_PKG
-else
+    else
     echo "ERROR: Can't install package(s). Verify correct package manager."
     exit 1;
-fi
+    fi
 }
 
 pkg_check_and_install
