b11fc186e43d439fda7a2187391e981291a615b6
notoriouslogank <logan.aker1989@gmail.com>
2023-06-14 23:25:10 -0400

    Made individual dotfile config functions; deprecated old .txt configs.

--- src/arkitekt.sh
+++ src/arkitekt.sh
@@ -43,10 +43,111 @@ function make_banner {
     echo $HOST | figlet -f slant > ssh_banner.txt
 }
 
+function make_motd {
+    cat << EOF > motd
+    #!/bin/bash
+    echo "Through me you pass into the city of woe:"
+    echo "Through me you pass into eternal pain:"
+    echo "Through me amoung the people lost for aye."
+    echo "Justice the founder of my fabric mov'd:"
+    echo "To rear me was the task of power divine,"
+    echo "Supremest wisdom, and primeval love."
+    echo "Before me things create were none, save things"
+    echo "Eternal, and eternal I endure."
+    echo "All hope abandon ye who enter here."
+EOF
+}
+
+function make_neofetch {
+    cat << EOF > neofetch
+    echo '    print_info() {'
+    echo '        info title'
+    echo '        info local_ip'
+    echo '        info uptime'
+    echo '        info underline'
+    echo ''
+    echo '        info "os" distro'
+    echo '        info "de" de'
+    echo '        info "wm" wm'
+    echo '        info "wm theme" wm_theme'
+    echo '        info "terminal" term'
+    echo '        info "shell" shell'
+    echo '        info "font" term_font'
+    echo '        info underline'
+    echo ''
+    echo '        info "cpu" cpu'
+    echo '        info "cpu" cpu_usage'
+    echo '        info "gpu" gpu'
+    echo '        info "aforementioned driver" gpu_driver'
+    echo '        info "disk" disk'
+    echo '        info "ram" memory'
+    echo '        info "resolution" resolution'
+    echo '        info underline'
+    echo ''
+    echo '        info "public ip" public_ip'
+    echo '    }'
+    echo ''
+    echo '    title_fqdn="off"'
+    echo '    uptime_shorthand="off"'
+    echo '    public_ip_host="http://ident.me"'
+    echo '    public_ip_timeout=2'
+    echo '    disk_show=('/')'
+    echo '    disk_subtitle="dir"'
+    echo '    disk_percent="on"'
+    echo '    music_player="auto"'
+    echo '    song_format='%artist% - %album% - %title%''
+    echo '    song_shorthand="off"'
+    echo '    mpc_args=()'
+    echo ''
+    echo '    # TEXT'
+    echo '    colors=(4 6 4 8 4 6)'
+    echo '    bold="off"'
+    echo '    underline_enabled="on"'
+    echo '    underline_char="+"'
+    echo '    separator="+"'
+    echo ''
+    echo '    block_range=(0 9)'
+    echo '    color_blocks="off"'
+    echo '    block_width=3'
+    echo '    block_height=1'
+    echo '    col_offset="auto"'
+    echo ''
+    echo '    bar_char_elapsed="+"'
+    echo '    bar_char_total="-"'
+    echo '    bar_border="on"'
+    echo '    bar_length=10'
+    echo '    bar_color_elapsed="4"'
+    echo '    bar_color_total="4"'
+    echo ''
+    echo '    # INFO'
+    echo '    cpu_display="barinfo"'
+    echo '    memory_display="barinfo"'
+    echo '    battery_display="off"'
+    echo '    disk_display="barinfo"'
+    echo ''
+    echo '    # IMAGE'
+    echo '    image_backend="ascii"'
+    echo '    image_source="ascii"'
+    echo '    ascii_distro="auto"'
+    echo '    ascii_colors=(4 8)'
+    echo '    ascii_bold="on"'
+    echo '    image_loop="off"'
+    echo '    crop_mode="normal"'
+    echo '    crop_offset="center"'
+    echo '    image_size="35%"'
+    echo '    gap=2'
+    echo '    yoffset=0'
+    echo '    xoffset=0'
+    echo '    background_color='
+    echo '    '
+    echo '    # STDOUT'
+    echo '    stdout="off"'
+EOF
+}
+
 function make_dotfiles {
-    # neofetch
-    cp neofetch.txt ~/.config/neofetch/config.conf
-    # ssh_banner
+    make_motd
+    make_neofetch
     make_banner
     # bashrc
     cp bashrc.txt ~/.bashrc
@@ -60,4 +161,5 @@ function make_dotfiles {
 backup_dotfiles
 pkg_check_and_install
 make_banner
+make_motd
 make_dotfiles
